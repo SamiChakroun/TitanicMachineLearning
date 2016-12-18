c5eaa31a2cd5ed7d91c96547bd32557856da7164
SamiChakroun <samii.chakroun@gmail.com>
2016-12-18 14:24:04 +0200

    cross validation for glm

--- step8.R
+++ step8.R
@@ -6,13 +6,13 @@
   
   #create a logistic regression model with the training data
   
-  #Predicting survival based on passenger the 4 features we picked in feature selection
+  #Predicting survival based on passenger the 4 features we picked using best subset selection
   # Title, Pclass, FamilySize and Sex
-  logistic.model = glm(Survived ~Title+Pclass+FamilySize+Sex, family = "binomial", data=data.train)
+  logistic.model = glm(Survived~Title+Pclass+FamilySize+Sex, family = "binomial", data=data.train)
   
   #Plot the model
   plot(logistic.model)
-  
+
   #Summary
   summary(logistic.model)
   
@@ -36,3 +36,37 @@
   
   #Conclusion: Logistic regression is fast but its not accurate enough in predicting survival.
   #It is better in this case to stick to Random Forest since it gave us a much better accuracy.
+  
+
+  #===============================
+  # Cross validation of glm
+  #===============================
+  
+  #In order to make our results more accurate, we need to perform cross validation on our logistical regression model
+  
+  #Load caret library
+  library(caret)
+  
+  cv.folds.10 = createMultiFolds(regfit.full, k = 10, times = 10)
+  
+  # Set up caret's trainControl object per above.
+  control.2 = trainControl(method = "repeatedcv", number = 10, repeats = 10)
+  
+  # Drop the NA factor
+  data.train$Survived = factor(data.train$Survived)
+  
+  # Set seed for reproducibility and train
+  set.seed(1111)
+  glm.10.cv.1 = train(x = data.train[,-1], y = data.train$Survived, method ="glm", family="binomial", trControl = control.2)
+  
+  # Check the results
+  glm.10.cv.1
+  
+  # Another way to see the results and calculate accuracy mannually
+  PredTrain = predict(glm.10.cv.1, newdata=data.train, type="raw") 
+  table(data.train$Survived, PredTrain)
+  
+  #almost the same accuracy as given by bs.10.cv.1
+  accuracy = (1 - (86/485)) * 100
+  accuracy
+  
